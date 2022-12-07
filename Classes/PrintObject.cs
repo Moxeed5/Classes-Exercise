36b9bde5e78b7c20ce64c935740df9b140a281a0
Max Morgan <maxmmorgan93@gmail.com>
2022-12-07 12:19:16 -0600

    initial commit

---
+++ Classes/PrintObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class PrintObject
+    {
+        public static void PrintCar(Car car)
+        {
+            Console.WriteLine($"The model of the car is: {car.Make}");
+            Console.WriteLine($"The make of the car is: {car.Model}");
+            Console.WriteLine($"The year of the car is: {car.Year}");
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Classes
+﻿using System.Diagnostics;
+
+namespace Classes
 {
     public class Program
     {
+
+        //Create a new instance of the Car class
+        //Set values in the properties for the object
+        //Print the values of each property to the Console
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+
+            car1.Make = "Volvo";
+            car1.Model = "240";
+            car1.Year = 1993;
+
+            PrintObject.PrintCar(car1);
         }
     }
 }
